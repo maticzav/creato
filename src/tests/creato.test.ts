11285fb4e454c0aa9748dcbf81a7db5a9ccfadd6
maticzav <matic.zavadlal@gmail.com>
2018-12-11 17:54:57 +0100

    tests, logo

--- src/tests/creato.test.ts
+++ src/tests/creato.test.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as mkdirp from 'mkdirp'
 
-import creato from '../'
+import { creato } from '../'
 
 import * as loader from '../loader'
 
